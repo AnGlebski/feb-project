49caf9cdd97bf3f260977d25e6325307fed4614f
AnGlebski <nastya.gluhova40@gmail.com>
2023-02-14 21:21:09 +0300

    init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,317 @@
+export const users = [
+    {
+      "_id": "63eba4253f11e4dcac2c9c88",
+      "index": 0,
+      "guid": "363a61b6-da71-49cb-a271-15f4c5c191ec",
+      "isActive": false,
+      "balance": "$3,218.17",
+      "picture": "http://placehold.it/32x32",
+      "age": 26,
+      "eyeColor": "green",
+      "name": "Herrera Conner",
+      "gender": "male",
+      "company": "SUNCLIPSE",
+      "email": "herreraconner@sunclipse.com",
+      "phone": "+1 (875) 502-2614",
+      "address": "906 Forrest Street, Staples, West Virginia, 4286",
+      "about": "Elit ullamco deserunt veniam reprehenderit duis. Aliquip esse aute ad ad incididunt excepteur esse exercitation velit qui elit. Dolore sit duis cillum proident adipisicing incididunt sunt tempor Lorem dolore proident non exercitation ea. Fugiat proident ex minim nulla elit do ad magna. Ex laborum laborum et consequat quis deserunt aute.\r\n",
+      "registered": "2017-07-21T09:02:21 -03:00",
+      "latitude": 35.837484,
+      "longitude": 89.143385,
+      "tags": [
+        "sit",
+        "laboris",
+        "reprehenderit",
+        "qui",
+        "proident",
+        "quis",
+        "ex"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Valdez Pacheco"
+        },
+        {
+          "id": 1,
+          "name": "Janine Henderson"
+        },
+        {
+          "id": 2,
+          "name": "Marissa Rodriguez"
+        }
+      ],
+      "greeting": "Hello, Herrera Conner! You have 9 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "63eba4257e978054ffd1d045",
+      "index": 1,
+      "guid": "69debab2-f0cb-4915-96d2-3dd364a02ebd",
+      "isActive": true,
+      "balance": "$3,959.47",
+      "picture": "http://placehold.it/32x32",
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Tabatha Washington",
+      "gender": "female",
+      "company": "ZENOLUX",
+      "email": "tabathawashington@zenolux.com",
+      "phone": "+1 (950) 482-3736",
+      "address": "783 Lawrence Avenue, Walton, Michigan, 2235",
+      "about": "Laborum eu laboris esse ad enim. Amet elit exercitation occaecat sunt esse duis. Pariatur deserunt do minim eiusmod consequat excepteur pariatur adipisicing ullamco dolore reprehenderit occaecat nisi eiusmod. Magna mollit irure nostrud voluptate non commodo amet cillum mollit consectetur aliqua cillum.\r\n",
+      "registered": "2019-06-15T10:18:03 -03:00",
+      "latitude": 79.665888,
+      "longitude": 2.134605,
+      "tags": [
+        "pariatur",
+        "exercitation",
+        "amet",
+        "ex",
+        "eiusmod",
+        "laborum",
+        "sunt"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Holly Owens"
+        },
+        {
+          "id": 1,
+          "name": "Concepcion Stanton"
+        },
+        {
+          "id": 2,
+          "name": "Jimmie Mejia"
+        }
+      ],
+      "greeting": "Hello, Tabatha Washington! You have 9 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "63eba4255740c25d4c0ad0f3",
+      "index": 2,
+      "guid": "b985e9f1-68aa-4d21-8e2e-69d0344ff89f",
+      "isActive": true,
+      "balance": "$1,963.05",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Mcclure Bennett",
+      "gender": "male",
+      "company": "TRIPSCH",
+      "email": "mcclurebennett@tripsch.com",
+      "phone": "+1 (978) 560-2174",
+      "address": "180 Sutton Street, Alderpoint, Guam, 5086",
+      "about": "Do nulla esse nulla reprehenderit excepteur eiusmod commodo excepteur. Cillum aute ex sit ut. Labore ad eu ea incididunt in quis cillum amet magna. Sunt minim eiusmod voluptate est magna labore aliqua nostrud ad qui sunt mollit ut. Exercitation exercitation nulla anim voluptate minim laborum. Reprehenderit ex elit sint ex.\r\n",
+      "registered": "2021-10-20T06:23:09 -03:00",
+      "latitude": -1.365614,
+      "longitude": 144.043519,
+      "tags": [
+        "nisi",
+        "nisi",
+        "consequat",
+        "ad",
+        "magna",
+        "labore",
+        "aute"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Johns Johns"
+        },
+        {
+          "id": 1,
+          "name": "Chambers Jarvis"
+        },
+        {
+          "id": 2,
+          "name": "Sylvia Sampson"
+        }
+      ],
+      "greeting": "Hello, Mcclure Bennett! You have 2 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "63eba4250b74777282f4d807",
+      "index": 3,
+      "guid": "f77dafce-d872-4543-bead-e779a05eb0b5",
+      "isActive": false,
+      "balance": "$3,481.56",
+      "picture": "http://placehold.it/32x32",
+      "age": 32,
+      "eyeColor": "brown",
+      "name": "Melisa Carpenter",
+      "gender": "female",
+      "company": "STEELTAB",
+      "email": "melisacarpenter@steeltab.com",
+      "phone": "+1 (879) 407-2845",
+      "address": "938 Joralemon Street, Eagleville, Pennsylvania, 457",
+      "about": "Aliquip eu cupidatat nisi commodo ipsum et amet. Occaecat exercitation ad proident magna exercitation. Proident esse qui cillum id non. Lorem proident eu et quis consectetur laboris nostrud consectetur laboris.\r\n",
+      "registered": "2017-11-06T03:25:09 -03:00",
+      "latitude": -22.127033,
+      "longitude": -141.394598,
+      "tags": [
+        "amet",
+        "exercitation",
+        "velit",
+        "non",
+        "exercitation",
+        "elit",
+        "proident"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Gonzalez Chapman"
+        },
+        {
+          "id": 1,
+          "name": "Combs Norton"
+        },
+        {
+          "id": 2,
+          "name": "Liza Mcfarland"
+        }
+      ],
+      "greeting": "Hello, Melisa Carpenter! You have 10 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "63eba425285927671747c79e",
+      "index": 4,
+      "guid": "fb7be99c-7a64-4db3-919c-5bebde3cfcd6",
+      "isActive": false,
+      "balance": "$3,663.77",
+      "picture": "http://placehold.it/32x32",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Marisol Gaines",
+      "gender": "female",
+      "company": "SARASONIC",
+      "email": "marisolgaines@sarasonic.com",
+      "phone": "+1 (995) 599-3319",
+      "address": "349 Kimball Street, Gerber, Nevada, 3479",
+      "about": "Culpa ullamco pariatur minim ut anim ad qui commodo sint magna labore eu reprehenderit. Laboris in nulla qui esse amet incididunt ex aliqua sit laborum elit enim. Esse id cupidatat excepteur non irure cillum magna labore eiusmod consectetur fugiat reprehenderit. Elit exercitation dolor laborum velit.\r\n",
+      "registered": "2015-08-25T04:02:46 -03:00",
+      "latitude": 55.79497,
+      "longitude": 21.670679,
+      "tags": [
+        "eiusmod",
+        "nisi",
+        "elit",
+        "aliqua",
+        "ea",
+        "labore",
+        "consequat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Chandra Hawkins"
+        },
+        {
+          "id": 1,
+          "name": "Snyder Arnold"
+        },
+        {
+          "id": 2,
+          "name": "Kay Barlow"
+        }
+      ],
+      "greeting": "Hello, Marisol Gaines! You have 8 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "63eba425c22981beca7cf948",
+      "index": 5,
+      "guid": "cdda2c8a-25ef-4199-b422-d006b8164891",
+      "isActive": true,
+      "balance": "$1,704.34",
+      "picture": "http://placehold.it/32x32",
+      "age": 37,
+      "eyeColor": "brown",
+      "name": "Vonda Peters",
+      "gender": "female",
+      "company": "GLEAMINK",
+      "email": "vondapeters@gleamink.com",
+      "phone": "+1 (864) 480-3879",
+      "address": "476 Bijou Avenue, Gibbsville, Illinois, 9108",
+      "about": "Qui occaecat consequat sit cillum excepteur est veniam id consectetur sint quis qui in nulla. Nostrud eiusmod veniam deserunt duis cupidatat consequat cupidatat eiusmod occaecat. Laborum adipisicing eu quis aute non anim occaecat labore eiusmod. Irure aute ullamco labore aliquip aliqua non commodo incididunt dolor labore do aute.\r\n",
+      "registered": "2018-10-30T01:46:53 -03:00",
+      "latitude": -28.160453,
+      "longitude": -117.525775,
+      "tags": [
+        "pariatur",
+        "velit",
+        "et",
+        "velit",
+        "nostrud",
+        "est",
+        "ut"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Lizzie Heath"
+        },
+        {
+          "id": 1,
+          "name": "Jensen Kirk"
+        },
+        {
+          "id": 2,
+          "name": "Mcknight Romero"
+        }
+      ],
+      "greeting": "Hello, Vonda Peters! You have 2 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "63eba42583bd516b12407602",
+      "index": 6,
+      "guid": "d592186d-7596-48c8-9e41-cd3fb9fbe810",
+      "isActive": false,
+      "balance": "$2,011.82",
+      "picture": "http://placehold.it/32x32",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Rose Maldonado",
+      "gender": "male",
+      "company": "RUGSTARS",
+      "email": "rosemaldonado@rugstars.com",
+      "phone": "+1 (897) 407-3884",
+      "address": "246 Irwin Street, Crawfordsville, Rhode Island, 6748",
+      "about": "Ipsum magna cupidatat sint do aliquip excepteur nulla minim consequat. Nulla est cillum ut fugiat excepteur amet consequat in veniam. Minim esse veniam dolore adipisicing labore quis minim voluptate aute exercitation. Cupidatat proident enim incididunt id laboris ad enim.\r\n",
+      "registered": "2020-12-10T03:49:52 -03:00",
+      "latitude": 77.621723,
+      "longitude": 3.28537,
+      "tags": [
+        "nostrud",
+        "ullamco",
+        "cillum",
+        "officia",
+        "occaecat",
+        "ea",
+        "ad"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Whitaker Hernandez"
+        },
+        {
+          "id": 1,
+          "name": "Madden Peck"
+        },
+        {
+          "id": 2,
+          "name": "Fletcher Parker"
+        }
+      ],
+      "greeting": "Hello, Rose Maldonado! You have 6 unread messages.",
+      "favoriteFruit": "strawberry"
+    }
+  ]
