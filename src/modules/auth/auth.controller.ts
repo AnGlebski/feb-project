450499e7d93d4421d04d562d37cb26b24e96cba7
AnGlebski <gluhova19962010@gmail.ru>
2023-04-21 20:42:36 +0300

    improved updata logic

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller, Post} from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, Get, Req} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDTO } from "../users/dto";
 import { UserLoginDTO } from "./dto";
 import { AuthUserResponse } from "./response";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { UsersService } from "../users/users.service";
+import { JwtAuthGuard } from "src/guards/jwt-guard";
+import { request } from "http";
 
 
 @Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService
+    ) {
   }
 
   @ApiTags('API')
@@ -23,5 +29,13 @@ export class AuthController {
   @Post("login")
   login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
     return this.authService.loginUser(dto);
+  }
+
+  //только авториз. пользователь
+  @UseGuards(JwtAuthGuard)
+  @Get('get-public-user-info')
+  getPublicUserInfo(@Req() request) {
+    const user = request.user
+    return this.userService.publicUser(user.email)
   }
 }
